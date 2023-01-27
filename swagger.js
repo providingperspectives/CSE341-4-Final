95e60e100c33bacc4415ffb17d2053705ca543fd
isiakpona-chuks <providingperspectivesent@gmail.com>
2023-01-27 18:57:18 +0100

    fix-1

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: 'My API',
     description: 'Contact API'
   },
-  host: 'cse341-instructor.herokuapp.com',
+  host: 'https://github.com/providingperspectives/CSE341-4-Final',
   schemes: ['https']
 };
 
